dd5681efcd3662732a87738dd8a1eb2c0b2c522e
Migue09 <raleb68@gmail.com>
2023-06-28 12:50:35 -0500

    [fix]

--- infraestructure/queries/beneficiarios/get_DiagnosticoList.js
+++ infraestructure/queries/beneficiarios/get_DiagnosticoList.js
@@ -4,10 +4,12 @@ const sqlQueries = require("../../sqlQueries/sql_Beneficiarios");
 const get_DiagnosticoList = async (diagnostico) => {
     try {
         let result;
-        if(diagnostico != undefined){
+        console.log(diagnostico);
+        if(diagnostico === undefined){
+            result = await pool.DBConnection.query(sqlQueries.GET_DIAGNOSTICOS_LIST);
+        }else{
             result = await pool.DBConnection.query(sqlQueries.GET_DIAGNOSTICOS_LIST_BUSQUEDA, [diagnostico]);
         }
-        result = await pool.DBConnection.query(sqlQueries.GET_DIAGNOSTICOS_LIST);
         return result.rows;
     } catch (error) {
         throw error;

f0b32da07f853b065b781a3c4572393e2392c761
Migue09 <raleb68@gmail.com>
2023-07-02 14:11:04 -0500

    [fix]

--- infraestructure/queries/beneficiarios/delete_Alergias.js
+++ infraestructure/queries/beneficiarios/delete_Alergias.js
@@ -3,7 +3,7 @@ const sqlQueries = require("../../sqlQueries/sql_Beneficiarios");
 
 const delete_Alergias = async (alergia) => {
     try{
-        const result = await pool.DBConnection.query(sqlQueries.DELETE_ALERGIA , [alergia.id_beneficiario, alergia.id_alergia, alergia.id_empleado]);
+        const result = await pool.DBConnection.query(sqlQueries.DELETE_ALERGIA , [alergia.id_beneficiario, alergia.id_alergia]);
         return result.rows;
     } catch(error){
         throw error;
